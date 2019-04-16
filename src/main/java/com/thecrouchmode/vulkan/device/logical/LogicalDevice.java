5623fb7014d90a88bfee9b6cbcbb093b2f7d6ee7
Markus <markus@celel.se>
2019-04-16 14:53:24 +0200

    more required code

--- src/main/java/com/thecrouchmode/vulkan/device/logical/LogicalDevice.java
+++ src/main/java/com/thecrouchmode/vulkan/device/logical/LogicalDevice.java
@@ -4,26 +4,36 @@ import com.thecrouchmode.vulkan.device.physical.PhysicalDevice;
 import com.thecrouchmode.vulkan.device.physical.QueueFamily;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.system.MemoryStack;
-import org.lwjgl.vulkan.VK10;
-import org.lwjgl.vulkan.VkDevice;
-import org.lwjgl.vulkan.VkDeviceCreateInfo;
+import org.lwjgl.vulkan.*;
 
 import java.util.List;
 
 public class LogicalDevice {
     VkDevice device;
 
-    public LogicalDevice(PhysicalDevice pd, List<QueueFamily> queueFamilies) {
+    public LogicalDevice(PhysicalDevice pd, List<QueueFamily> queueFamilies, PointerBuffer extensions) {
         try(var stack = MemoryStack.stackPush()) {
             VkDeviceCreateInfo deviceCreateInfo = VkDeviceCreateInfo.callocStack(stack);
 
+            VkDeviceQueueCreateInfo.Buffer queues = VkDeviceQueueCreateInfo.callocStack(queueFamilies.size(), stack);
+
+            for(QueueFamily qf : queueFamilies) {{
+                queues.put(qf.createInfo(stack));
+            }}
+
+            PointerBuffer layers = stack.callocPointer(1);
+            //layers.put(stack.UTF8("VK_LAYER_LUNARG_standard_validation"));
             deviceCreateInfo
                     .sType(VK10.VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO)
-                    .pQueueCreateInfos(null)
-                    .pEnabledFeatures(null);
+                    .pQueueCreateInfos(queues)
+                    .pEnabledFeatures(VkPhysicalDeviceFeatures.mallocStack(stack))
+                    //.ppEnabledLayerNames(layers.flip())
+                    .ppEnabledExtensionNames(extensions);
 
             PointerBuffer pointer = stack.pointers(1);
-            VK10.vkCreateDevice(pd.device, deviceCreateInfo, null, pointer);
+            int res = VK10.vkCreateDevice(pd.device, deviceCreateInfo, null, pointer);
+
+            device = new VkDevice(pointer.get(0), pd.device, deviceCreateInfo);
         }
 
     }
