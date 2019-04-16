a4513eca6f8c55a5bb7f7da3d803cae7f6e74d7f
Markus <markus@celel.se>
2019-04-16 14:52:59 +0200

    info creation for creating queues

--- src/main/java/com/thecrouchmode/vulkan/device/physical/QueueFamily.java
+++ src/main/java/com/thecrouchmode/vulkan/device/physical/QueueFamily.java
@@ -1,5 +1,8 @@
 package com.thecrouchmode.vulkan.device.physical;
 
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.vulkan.VK10;
+import org.lwjgl.vulkan.VkDeviceQueueCreateInfo;
 import org.lwjgl.vulkan.VkQueueFamilyProperties;
 
 import static org.lwjgl.vulkan.VK10.VK_QUEUE_COMPUTE_BIT;
@@ -10,12 +13,24 @@ public class QueueFamily {
     public final int index;
     public final boolean graphicsBit;
     public final boolean computeBit;
+    public float priority = 1;
 
     QueueFamily(int index, VkQueueFamilyProperties properties) {
         this.index = index;
         graphicsBit = (properties.queueFlags() & VK_QUEUE_GRAPHICS_BIT) != 0;
         computeBit = (properties.queueFlags() & VK_QUEUE_COMPUTE_BIT) != 0;
 
+    }
+
+    public VkDeviceQueueCreateInfo createInfo(MemoryStack stack) {
+        VkDeviceQueueCreateInfo queueCreateInfo = VkDeviceQueueCreateInfo.mallocStack(stack);
+
+        queueCreateInfo
+                .sType(VK10.VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO)
+                .queueFamilyIndex(index)
+                .pQueuePriorities(stack.floats(priority));
+
+        return queueCreateInfo;
     }
 
     @Override
