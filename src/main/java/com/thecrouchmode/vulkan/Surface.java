a2ed68d8053a584599bc3cca1499fa1d46f273e0
Markus <markus@celel.se>
2019-04-16 22:11:33 +0200

    added surface destruction

--- src/main/java/com/thecrouchmode/vulkan/Surface.java
+++ src/main/java/com/thecrouchmode/vulkan/Surface.java
@@ -2,9 +2,9 @@ package com.thecrouchmode.vulkan;
 
 import com.thecrouchmode.glfw.Window;
 import org.lwjgl.glfw.GLFWVulkan;
+import org.lwjgl.vulkan.KHRSurface;
 
 public class Surface {
-
 
     private final Instance instance;
     private final long surface;
@@ -15,5 +15,9 @@ public class Surface {
         long[] surface = {0};
         GLFWVulkan.glfwCreateWindowSurface(instance.instance, window.id, null, surface);
         this.surface = surface[0];
+    }
+
+    public void destroy() {
+        KHRSurface.vkDestroySurfaceKHR(instance.instance, surface, null);
     }
 }
