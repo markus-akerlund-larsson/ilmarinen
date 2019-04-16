a8efb8c1605b058f4180d2183b5dfc28bcd977e3
Markus <markus@celel.se>
2019-04-16 14:53:35 +0200

    wiring together components

--- src/main/java/com/thecrouchmode/vulkan/HelloTriangleApp.java
+++ src/main/java/com/thecrouchmode/vulkan/HelloTriangleApp.java
@@ -1,7 +1,13 @@
 package com.thecrouchmode.vulkan;
 
 import com.thecrouchmode.glfw.Window;
+import com.thecrouchmode.vulkan.device.logical.LogicalDevice;
+import com.thecrouchmode.vulkan.device.physical.PhysicalDevice;
+import com.thecrouchmode.vulkan.device.physical.QueueFamily;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HelloTriangleApp {
 
@@ -25,6 +31,16 @@ public class HelloTriangleApp {
 
     private void initVulkan() {
         instance = new Instance();
+
+        PhysicalDevice physicalDevice = instance.physicalDevices().get(0);
+
+        List<QueueFamily> qfs = physicalDevice.queueFamilies()
+                .stream()
+                .peek(qf-> System.out.println("Queue family: "+qf))
+                .filter(q->q.graphicsBit)
+                .limit(1)
+                .collect(Collectors.toList());
+        LogicalDevice logicalDevice = new LogicalDevice(physicalDevice, qfs, instance.enabledExtensions);
 
     }
 
